ece32266545df387e6dac19469969585ae31266f
punytan <punytan@gmail.com>
2010-12-11 01:42:31 +0900

    initial commit

---
+++ lib/Task/BeLike/Punytan.pm
@@ -0,0 +1,32 @@
+package Task::BeLike::Punytan;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Task::BeLike::Punytan -
+
+=head1 SYNOPSIS
+
+  use Task::BeLike::Punytan;
+
+=head1 DESCRIPTION
+
+Task::BeLike::Punytan is
+
+=head1 AUTHOR
+
+punytan E<lt>punytan@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
